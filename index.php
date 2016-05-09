7ff186cbf5aaf8c456271c064ec3afc3657f16bc
cfoordddd <cfoordddd@hotmail.com>
2016-05-09 18:34:56 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 This is just a test. <?=date('Y-m-d'); ?>
 <img src="images/tilde_logo.png" alt="Team Tilde" title="Team Tilde">
-Glenn was here.
+<p>Glenn was here.</p>
+<p>Clifford was here too.</p>
