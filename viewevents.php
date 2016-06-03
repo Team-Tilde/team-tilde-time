807693ace4f6853c63d985712de0f82571f3b0f8
Anthony La <anthony.la3@hotmail.com>
2016-06-03 16:43:39 +1000

    Slight fix with scripts in viewevents.php
    Error due to renaming of folder from js to scripts.
    
    viewevents.php was affected with the navigation bar.
    
    Fix changes include:
    
    Changing script paths from js to scripts.
    Also jquery is now grabbed not online but locally on the server.
    Version used jquery-1.9.1.min.js.

--- viewevents.php
+++ viewevents.php
@@ -298,8 +298,8 @@
 		</div>
 	</div>;
 	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+	<script src="scripts/jquery-1.9.1.min.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
 
 </body>
 
