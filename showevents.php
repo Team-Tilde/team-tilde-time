d9abbc6f650d4c5d8e59fc7832f12edb5683860e
Anthony La <anthony.la3@hotmail.com>
2016-06-10 18:03:39 +1000

    Sort by end time date for events.
    showevents.php:
    Changed query to sort by end time date by default.

--- showevents.php
+++ showevents.php
@@ -17,7 +17,8 @@
 	
 	$sql = "SELECT e.event_id, ec.description as ecdescription, e.title, e.description, e.location, e.date_time_start, e.date_time_end FROM Event as e
 			JOIN eventcategory as ec ON e.event_category_id = ec.event_category_id
-			WHERE task_id = '" . $selected . "'";
+			WHERE task_id = '" . $selected . "' " . 
+			"ORDER BY e.date_time_end";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
