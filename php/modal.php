eda4f054f4b4a109faec018d2ee709c56d6b7c0f
Anthony La <anthony.la3@hotmail.com>
2016-06-18 14:41:09 +1000

    Added modal.php
    Moved all modal code to modal.php to use for multiple pages.

---
+++ php/modal.php
@@ -0,0 +1,130 @@
+<div id="eventDetailModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Event Details</h4>
+			</div>
+			<div id="eventDetail" class="modal-body">
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="addEventModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Add event</h4>
+			</div>
+			<div class="modal-body">
+				<div id="addEventModalPlaceholder"></div>	
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="editEventModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Edit event</h4>
+			</div>
+			<div class="modal-body">
+				<div id="editEventModalPlaceholder"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="deleteEventModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Delete event</h4>
+			</div>
+			<div class="modal-body">
+				<div id="deleteEventModalPlaceholder"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="taskDetailModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Task Details</h4>
+				</div>
+				<div id="taskDetail" class="modal-body">
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<div id="addTaskModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Add task</h4>
+			</div>
+			<div class="modal-body">
+				<div id="addTaskModalPlaceholder"></div>	
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="editTaskModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Edit task</h4>
+			</div>
+			<div class="modal-body">
+				<div id="editTaskModalPlaceholder"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="deleteTaskModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Delete task</h4>
+			</div>
+			<div class="modal-body">
+				<div id="deleteTaskModalPlaceholder"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="addEventModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Add event</h4>
+			</div>
+			<div class="modal-body">
+				<div id="addEventModalPlaceholder"></div>	
+			</div>
+		</div>
+	</div>
+</div>
+
