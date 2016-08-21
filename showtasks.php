3c402c02bf8af4c305ce1cab9e9b3b8ed791a2ab
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-21 22:00:43 +1000

    Update showtasks.php

--- showtasks.php
+++ showtasks.php
@@ -8,7 +8,7 @@
 	}
 
 	$sql = "SELECT t.task_id, tc.description as tcdescription, t.description, t.task_category_id, t.date_time_start, t.date_time_end, tes.status FROM Task as t
-			JOIN taskcategory as tc ON t.task_category_id = tc.task_category_id
+			JOIN TaskCategory as tc ON t.task_category_id = tc.task_category_id
 			JOIN TaskEventStatus as tes ON t.task_event_status_id = tes.task_event_status_id
 			WHERE t.date_time_end > t.date_time_start"; //might be what tharanga wants
 			echo $sql;
