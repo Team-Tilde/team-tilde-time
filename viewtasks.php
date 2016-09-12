d504e52c1d34177019a8386432708989f6cb68ad
cfoordddd <cfoordddd@hotmail.com>
2016-09-12 16:27:01 +1000

    added task filter

--- viewtasks.php
+++ viewtasks.php
@@ -19,8 +19,29 @@
 		<?php
 			require_once "php/nav.php";
 		?>
+		<label>Filter Tasks:</label><select id="selectorTask" class='form-control' onchange='showTasksByFilter(this.value)'>
+		<option value='0'>All Tasks</option>
 		<?php
-			require_once "showtasks.php";
+			$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+			
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+		
+			$sql = "SELECT task_category_id, description FROM TaskCategory";
+			$result = $conn->query($sql);
+			
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					echo "<option value='" . $row['task_category_id'] . "'>" . $row['description'] . "</option>";
+				}
+			} else {
+				require_once "showtasks.php"; // static
+			}
+			
+			echo "</select>";
+			
+			$conn->close();
 		?>
 		<div id="taskData"></div>
 	</div>
